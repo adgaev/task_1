b84183b903fda70339deb28c828e698e747a033a
adgaev <galina.adolshina@gmail.com>
2018-05-14 16:56:51 +0300

    4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,37 @@
 package com.company;
 
-
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args) {
-        /* 1. Приветствовать любого пользователя при вводе его имени через командную строку */
+
+/*
+ 1. Приветствовать любого пользователя при вводе его имени через командную строку
+*/
         System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         System.out.println("Hi, " + name + "!");
 
-        /* 2. Отобразить в окне консоли аргументы командной строки в обратном порядке */
+
+/*
+ 2. Отобразить в окне консоли аргументы командной строки в обратном порядке
+*/
         System.out.println("Enter any value:");
         Scanner value = new Scanner(System.in);
         String mirror = value.nextLine();
 
         char[] mir = mirror.toCharArray();
-        System.out.print("Lets check it in the mirror: ");
+        System.out.println("Lets check it in the mirror: ");
         for (int i = mir.length - 1; i >= 0; i--) {
             System.out.print(mir[i]);
         }
 
-        /* 3. Вывести заданное количество случайных чисел с переходом и без перехода на новую строку */
-                System.out.println("Enter random number: ");
+
+/*
+ 3. Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+*/
+        System.out.println("Enter random number: ");
         Scanner number = new Scanner(System.in);
         String random = number.nextLine();
         char[] ran = random.toCharArray();
@@ -39,5 +45,24 @@ public class Main {
         for (int i = 0; i < ran.length; i++) {
             System.out.print(ran[i]);
         }
+
+/*
+ 4. Ввести пароль из командной строки и сравнить его со строкой-образцом
+*/
+
+        System.out.println("Enter the password:");
+        Scanner password = new Scanner(System.in);
+        String pas = password.next();
+        if (pas.equals("123456")){
+            System.out.println("Password is correct.");}
+            else
+        {
+            System.out.println("Password is wrong!");}
+        }
+/*
+5. Ввести целые числа как аргументы командной строки, подсчитать их суммы (произведения) и вывести результат на консоль
+*/
+
+
+
     }
-}
